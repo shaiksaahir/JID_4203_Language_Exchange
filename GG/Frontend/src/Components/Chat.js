5b611cf95331dc970b3af918aff7244c62a8006b
shaiksaahir <132622879+shaiksaahir@users.noreply.github.com>
2024-09-18 15:12:17 -0400

    Created Audio Preferences

--- GG/Frontend/src/Components/Chat.js
+++ GG/Frontend/src/Components/Chat.js
@@ -1,11 +1,11 @@
 import { useState, useEffect } from 'react';
-import { useRef} from "react";
+import { useRef } from "react";
 import "./Chat.css";
 import Conversation from "./Conversation";
 import ChatBox from "./ChatBox";
 import LogoSearch from "./LogoSearch";
 import { createSearchParams, useSearchParams, useNavigate, Navigate } from "react-router-dom";
-import {handleChatApi, handleGetUser} from '../Services/userService';
+import { handleChatApi, handleGetUser } from '../Services/userService';
 import { io } from "socket.io-client";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -13,7 +13,7 @@ const Chat = () => {
 
     const [chats, setChats] = useState([]);
     const [currentChat, setCurrentChat] = useState(null);
-    const[search] = useSearchParams();
+    const [search] = useSearchParams();
     const senderId = search.get("senderid"); //login user_id
     const user = handleGetUser(senderId); //login user info
     const socket = useRef();
@@ -25,8 +25,8 @@ const Chat = () => {
     useEffect(() => {
         const getChats = async () => {
             try {
-            //data = object of ChatModel {chatId, senderId, receiverId}
-                const data  = await handleChatApi(senderId);
+                //data = object of ChatModel {chatId, senderId, receiverId}
+                const data = await handleChatApi(senderId);
                 setChats(data.chatsData);
             } catch (error) {
                 console.log(error);
@@ -35,55 +35,53 @@ const Chat = () => {
         getChats();
     }, [senderId]);
 
-     // Connect to Socket.io
+    // Connect to Socket.io
     useEffect(() => {
         socket.current = io("ws://localhost:8800");
         socket.current.emit("new-user-add", senderId);
         socket.current.on("get-users", (users) => {
-          setOnlineUsers(users);
+            setOnlineUsers(users);
         });
     }, [user]);
 
     // Send Message to socket server
     useEffect(() => {
-        if (sendMessage!==null) {
-          socket.current.emit("send-message", sendMessage);}
+        if (sendMessage !== null) {
+            socket.current.emit("send-message", sendMessage);
+        }
     }, [sendMessage]);
 
-  // Get the message from socket server
-  useEffect(() => {
-    socket.current.on("recieve-message", (data) => {
-      setReceivedMessage(data);
-    });
+    // Get the message from socket server
+    useEffect(() => {
+        socket.current.on("recieve-message", (data) => {
+            setReceivedMessage(data);
+        });
 
-  }, []);
+    }, []);
 
-   const checkOnlineStatus = (chat) => {
-      const chatMember = chat["receiverId"];
-      console.log(onlineUsers);
-      const online = onlineUsers.find((user) => user.userId == chatMember);
-      return online ? true : false;
-   };
+    const checkOnlineStatus = (chat) => {
+        const chatMember = chat["receiverId"];
+        console.log(onlineUsers);
+        const online = onlineUsers.find((user) => user.userId == chatMember);
+        return online ? true : false;
+    };
 
-   return (
+    return (
         <div className="Chat">
             {/* Left Side */}
             <div className="Left-side-chat">
-            <LogoSearch />
+                <LogoSearch />
                 <div className="Chat-container">
                     <h2>Chats</h2>
                     <div className="Chat-list">
                         {chats.map((chat) => (
-                        <div onClick={() => {
-                        setCurrentChat(chat);
-                        }}
-                        >
-                            <Conversation
-                              data={chat} // send prop data to Conversation - data
-                              currentUserId={senderId} // user_id that currently login and send as prop to Conversation - currentUserId
-                              online={checkOnlineStatus(chat)}
-                            />
-                        </div>
+                            <div key={chat.chatId} onClick={() => setCurrentChat(chat)}>
+                                <Conversation
+                                    data={chat} // send prop data to Conversation - data
+                                    currentUserId={senderId} // user_id that currently login and send as prop to Conversation - currentUserId
+                                    online={checkOnlineStatus(chat)}
+                                />
+                            </div>
                         ))}
                     </div>
                 </div>
@@ -91,12 +89,27 @@ const Chat = () => {
 
             {/* Right Side */}
             <div className="Right-side-chat">
-            <ChatBox
-              chat={currentChat}
-              currentUser={senderId}
-              setSendMessage={setSendMessage}
-              receivedMessage={receivedMessage}
-              />
+                <ChatBox
+                    chat={currentChat}
+                    currentUser={senderId}
+                    setSendMessage={setSendMessage}
+                    receivedMessage={receivedMessage}
+                />
+                {/* Audio Preferences Section */}
+                <div className="audio-preferences">
+                    <h3>Audio Preferences</h3>
+                    <div className="mute-section">
+                        <button className="mute-btn">Mute</button>
+                    </div>
+                    <div className="mic-selection">
+                        <label htmlFor="mic">Select Microphone:</label>
+                        <select id="mic" name="mic">
+                            <option value="default">Default Microphone</option>
+                            <option value="mic1">Microphone 1</option>
+                            <option value="mic2">Microphone 2</option>
+                        </select>
+                    </div>
+                </div>
             </div>
         </div>
     );
